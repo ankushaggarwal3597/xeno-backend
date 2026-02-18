26c5b125e1c8e8855371ddb26f83dbfd4a511876
Ankush Aggarwal <ankushaggarwal3597@gmail.com>
2026-02-18 20:24:25 +0530

    postgres migration

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+/*require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
@@ -18,5 +18,27 @@ const sequelize = new Sequelize(
     }
   }
 );
+
+module.exports = sequelize;*/
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
 
 module.exports = sequelize;
